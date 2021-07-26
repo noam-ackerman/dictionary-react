a9b4722467fc199b47411cd1748570d446b8e439
noam-ackerman <noamac1812@gmail.com>
2021-07-26 15:04:46 +0200

    fixing class name

--- src/Results.js
+++ src/Results.js
@@ -8,7 +8,7 @@ export default function Results (props) {
 <div className="results">
     <div className="word">{props.data.word}</div>
   <div className="phonetics"> 
-  <a className="audio" href={props.data.phonetics[0].audio} target="_blank" rel="noreferrer"><i class="fas fa-volume-up"></i></a><span className="text">{props.data.phonetics[0].text}</span>
+  <a className="audio" href={props.data.phonetics[0].audio} target="_blank" rel="noreferrer"><i className="fas fa-volume-up"></i></a><span className="text">{props.data.phonetics[0].text}</span>
   </div>
     {props.data.meanings.map(function(meaning,index) {
         return ( <div key={index}>
