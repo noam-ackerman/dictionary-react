7ef3695b64ce76274cc7f591630910a068dcea34
noam-ackerman <noamac1812@gmail.com>
2021-07-26 14:22:12 +0200

    adding synonyms display

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 import "./Results.css";
+
 
 export default function Meaning (props) {
     return <div className="meaning">
@@ -8,9 +10,10 @@ export default function Meaning (props) {
        </div>
        {props.meaning.definitions.map(function(definition, index){
            return (
-               <div key={index}>
-                     <div className="definition">{definition.definition}</div>
+               <div key={index}> 
+                   <div className="definition">{definition.definition}</div>
         <div className="example"><em>{definition.example}</em></div>
+        <Synonyms synonyms={definition.synonyms}/>
        </div>
            );
        })}
---
+++ src/Synonyms.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Results.css";
+
+export default function Synonyms (props) {
+    if (props.synonyms) {
+    return (
+        <div>
+<div className="synonymsTitle">
+    Similar:
+    </div>
+    <div className="synonymsList">
+    {props.synonyms.map((synonym,index) => {
+        return  <span key={index} className="synonym">{synonym} <strong>•</strong> </span> ;
+    })}
+</div>
+</div>
+
+    );
+} else {
+    return null;
+}
+}
