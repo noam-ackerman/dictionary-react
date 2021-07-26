ef0ce14c785dd3de2ed46d983c897d826619d4c5
noam-ackerman <noamac1812@gmail.com>
2021-07-26 17:32:01 +0200

    fixing syntax of map functions

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,20 +2,21 @@ import React from "react";
 import Synonyms from "./Synonyms";
 import "./Results.css";
 
-
-export default function Meaning (props) {
-    return <div className="meaning">
-       <div className="partOfSpeech">
-           {props.meaning.partOfSpeech}
-       </div>
-       {props.meaning.definitions.map(function(definition, index){
-           return (
-               <div key={index}> 
-                   <div className="definition">{definition.definition}</div>
-        <div className="example"><em>{definition.example}</em></div>
-        <Synonyms synonyms={definition.synonyms}/>
-       </div>
-           );
-       })}
+export default function Meaning(props) {
+  return (
+    <div className="meaning">
+      <div className="partOfSpeech">{props.meaning.partOfSpeech}</div>
+      {props.meaning.definitions.map((definition, index) => {
+        return (
+          <div key={index}>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">
+              <em>{definition.example}</em>
+            </div>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
+  );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React from "react";
 import Meaning from "./Meaning";
 import Photos from "./Photos";
- import "./Results.css";
+import "./Results.css";
 
-export default function Results (props) {
-    if(props.data !== null) {
-           return (
-<div className="results">
-    <div className="word">{props.data.word}</div>
-  <div className="phonetics"> 
-  <a className="audio" href={props.data.phonetics[0].audio} target="_blank" rel="noreferrer"><i className="fas fa-volume-up"></i></a><span className="text">{props.data.phonetics[0].text}</span>
-  </div>
-    {props.data.meanings.map(function(meaning,index) {
-        return ( <div key={index}>
-            <Meaning meaning={meaning}/>
-            </div>);
-    })}
-    <Photos photos={props.photos}/>
-    </div>
+export default function Results(props) {
+  if (props.data !== null) {
+    return (
+      <div className="results">
+        <div className="word">{props.data.word}</div>
+        <div className="phonetics">
+          <a
+            className="audio"
+            href={props.data.phonetics[0].audio}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fas fa-volume-up"></i>
+          </a>
+          <span className="text">{props.data.phonetics[0].text}</span>
+        </div>
+        {props.data.meanings.map((meaning, index) => {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+        <Photos photos={props.photos} />
+      </div>
     );
-    } else {
-        return null;
-    }
+  } else {
+    return null;
+  }
 }
